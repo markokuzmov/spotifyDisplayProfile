c7e7c635e338294abf9416c4ddb913407eef6df3
markokuzmov <marko.kuzmov@gmail.com>
2025-08-05 20:11:50 -0400

    added top artists and top tracks, created links for tracks, updated css, modified redirect_uri

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 const clientId = import.meta.env.VITE_CLIENT_ID
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
-const redirect_uri = 'http://127.0.0.1:3000/callback'
+const redirect_uri = `${window.location.origin}/callback`
 
 const generateCodeVerifier = (length) => {
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -28,7 +28,7 @@ const redirectToAuthCodeFlow = async () => {
         client_id: clientId,
         response_type: 'code',
         redirect_uri: redirect_uri,
-        scope: "user-read-private user-read-email",
+        scope: "user-read-private user-read-email user-top-read",
         code_challenge_method: "S256",
         code_challenge: challenge
     });
@@ -113,12 +113,23 @@ const fetchWithAuth = async (URL, options = {}) => {
     return result
 }
 
-async function fetchProfile() {
+const fetchProfile = async () => {
     const result = await fetchWithAuth("https://api.spotify.com/v1/me") 
     return result.json()
 }
 
-function populateUI(profile) {
+const fetchTopArtists = async () => {
+    const result = await fetchWithAuth("https://api.spotify.com/v1/me/top/artists?time_range=short_term")
+    return result.json()
+}
+
+const fetchTopTracks = async () => {
+    const result = await fetchWithAuth("https://api.spotify.com/v1/me/top/tracks?time_range=short_term")
+    return result.json()
+}
+
+function populateProfileUI(profile) {
+
     document.getElementById("displayName").innerText = profile.display_name;
     if (profile.images[0]) {
         const profileImage = new Image(200, 200);
@@ -132,6 +143,38 @@ function populateUI(profile) {
     document.getElementById("url").setAttribute("href", profile.external_urls.spotify);
     document.getElementById("uri").innerText = profile.href;
     document.getElementById("uri").setAttribute("href", profile.href);
+
+}
+
+function populateTopItemsUI(topArtists, topTracks) {
+    var artistList = document.getElementById("topArtists")
+    var trackList = document.getElementById("topTracks")
+
+    for (var artist of topArtists.items) {
+        var li = document.createElement('li')
+        var artistImage = new Image(50, 50)
+        artistImage.src = artist.images[0].url
+        li.appendChild(artistImage)
+        li.appendChild(document.createTextNode(artist.name))
+        artistList.appendChild(li)
+    }
+
+    for (var track of topTracks.items) {
+        var li = document.createElement('li')
+
+        var trackImage = new Image(50, 50)
+        trackImage.src = track.album.images[0].url
+        li.appendChild(trackImage)
+        li.appendChild(document.createTextNode(track.name))
+
+        var link = document.createElement('a')
+        link.setAttribute("href", track.external_urls.spotify)
+        link.setAttribute("target", "_blank")
+        link.appendChild(li)
+        
+        trackList.appendChild(link)
+    }
+
 }
 
 if (!code) {
@@ -142,6 +185,11 @@ if (!code) {
     window.history.replaceState({}, document.title, redirect_uri);
 
     const profile = await fetchProfile()
-    populateUI(profile)
-    console.log(profile)
+    const topArtists = await fetchTopArtists()
+    const topTracks = await fetchTopTracks()
+
+    console.log(profile, topArtists, topTracks)
+
+    populateProfileUI(profile)
+    populateTopItemsUI(topArtists, topTracks)
 }
